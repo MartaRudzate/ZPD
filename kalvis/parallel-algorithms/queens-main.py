eed622ed1892c7273fb58ec9e7a9512fa895f64d
Kalvis Apsitis <kapsitis@whitecryption.com>
2022-09-30 19:34:47 +0300

    cmd-line argument added

--- kalvis/parallel-algorithms/queens-main.py
+++ kalvis/parallel-algorithms/queens-main.py
@@ -1,9 +1,15 @@
 from queens import *
+import sys
 
 # We could call this function for N = 4,5,...,14 and log the runtime
 # After that we can optimize the code and visualize the speed improvements.
-def main():
-    printAll(8)
+def main(N):
+    printOne(N)
 
 if __name__ == '__main__':
-    main()
+    if (len(sys.argv) <= 1):
+        print('Usage: python queens-main.py <N>')
+        exit(0)
+    # main()
+    aa = sys.argv[1]
+    main(int(aa))
