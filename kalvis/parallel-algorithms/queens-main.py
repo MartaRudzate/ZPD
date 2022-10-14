dcf03fcb3f5eaa1beaa2933c35f44ee703083846
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2022-10-15 01:49:35 +0300

    Object Oriented

--- kalvis/parallel-algorithms/queens-main.py
+++ kalvis/parallel-algorithms/queens-main.py
@@ -2,31 +2,48 @@ from queens import *
 import sys
 import time
 
-# We could call this function for N = 4,5,...,14 and log the runtime
-# After that we can optimize the code and visualize the speed improvements.
-def main():
-    if (len(sys.argv) <= 2):
-        print('Usage: python queens-main.py <N> <M>')
-        exit(0)
-    # main()
-    N1 = int(sys.argv[1])
-    N2 = int(sys.argv[2])
 
+# Call this for N = 4,5,...,14 (find the first valid queen placement) and log the runtime in milliseconds.
+def main():
+    if len(sys.argv) <= 2:
+        print('Usage: python queens-main.py <n1> <n2>')
+        exit(0)
+    n1 = int(sys.argv[1])
+    n2 = int(sys.argv[2])
     computation_times = dict()
 
-    for N in range(N1, N2+1):
+    for n in range(n1, n2+1):
         start_time = time.time()
-        printOne(N)
+
+        board = [[0] * n for k in range(0, n)]
+        solve_nqueens(board, 0)
+        print_solution(board)
+        # qp = QueenPosition(n)
+        # qp.findFirst(0)
+        # qp.printLatestSolution()
+
         end_time = time.time()
-        print('---{}: {:.3f} seconds ---'.format(N, end_time - start_time))
-        computation_times[N] = round(1000*(end_time - start_time))
+        print('---{}: {:.3f} seconds ---'.format(n, end_time - start_time))
+        computation_times[n] = round(1000*(end_time - start_time))
 
     with open('computation_times2.txt', 'a') as file_object:
         file_object.write('N,milliseconds\n')
-        for N in range(N1, N2+1):
-            file_object.write("{},{}\n".format(N, computation_times[N]))
+        for n in range(n1, n2+1):
+            file_object.write("{},{}\n".format(n, computation_times[n]))
 
+# This function prints all valid queen placements on a N*N chessboard in a compact form
+def outputAllQueenPositions(N):
+    qp2 = QueenPosition(N)
+    all_boards = []
+    while qp2.findNextPosition(0):
+        all_boards.append(qp2.latestSolution)
+    for bb in all_boards:
+        print(bb)
+    print('all_boards count: {}'.format(len(all_boards)))
 
 if __name__ == '__main__':
-    printAll(8)
-    #main()
+    main()
+    # outputAllQueenPositions(8)
+
+
+
--- kalvis/parallel-algorithms/queens.py
+++ kalvis/parallel-algorithms/queens.py
@@ -1,110 +1,131 @@
+import copy
+
 # Adapted from https://www.geeksforgeeks.org/n-queen-problem-backtracking-3/
 
-def printSolution(board):
+
+class QueenPosition:
+    board = []
+    n = 1
+    latestSolution = []
+    breadcrumbs = []
+
+    def __init__(self, N, latestSolution=None):
+        self.n = N
+        self.board = [[0] * self.n for k in range(0, self.n)]
+        self.breadcrumbs = [-1] * self.n
+        if latestSolution is None:
+            self.latestSolution = [-1] * self.n
+        else:
+            self.latestSolution = copy.deepcopy(latestSolution)
+
+    def printLatestSolution(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                print(int(self.latestSolution[i] == j), end=" ")
+            print()
+
+    def isSafe(self, row, col):
+        # Check this row on left side
+        for i in range(col):
+            if self.board[row][i] == 1:
+                return False
+        # Check upper diagonal on left side
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if self.board[i][j] == 1:
+                return False
+        # Check lower diagonal on left side
+        for i, j in zip(range(row, self.n, 1), range(col, -1, -1)):
+            if self.board[i][j] == 1:
+                return False
+        return True
+
+    def findFirst(self, col):
+        if col >= self.n:
+            self.latestSolution = self.compactifySolution()
+            return True
+        for i in range(self.n):
+            if self.isSafe(i, col):
+                self.board[i][col] = 1
+                if self.findFirst(col + 1):
+                    return True
+                self.board[i][col] = 0
+        return False
+
+    def compactifySolution(self):
+        result = [-1] * self.n
+        for col in range(0, self.n):
+            for i in range(0, self.n):
+                if self.board[i][col] == 1:
+                    result[col] = i
+        return result
+
+    def getLatestSolution(self):
+        return self.latestSolution
+
+    def findNextPosition(self, col):
+        if col >= self.n:
+            self.latestSolution = self.compactifySolution()
+            self.breadcrumbs = copy.deepcopy(self.latestSolution)
+            self.board = [[0] * self.n for k in range(0, self.n)]
+            return True
+
+        for i in range(self.n):
+            if i < self.breadcrumbs[col]:
+                continue
+            if i == self.breadcrumbs[col] and col == self.n - 1:
+                continue
+            if self.isSafe(i, col):
+                # Place this queen in board[i][col]
+                self.board[i][col] = 1
+                # recursive call - an attempt to place remaining queens
+                if self.findNextPosition(col + 1):
+                    return True
+                # If there is no solution on row "i", remove queen from board[i][col]
+                self.board[i][col] = 0
+
+        # The queen can not be placed in any row in this column
+        self.breadcrumbs = [-1] * self.n
+        return False
+
+
+def print_solution(board):
     n = len(board)  # size of the chessboard - number of rows
     for i in range(n):
         for j in range(n):
             print(board[i][j], end=" ")
         print()
 
-# A utility function to check if a queen can
-# be placed on board[row][col].
-# (Assume that "col" queens are already placed in columns from 0 to col -1.
-def isSafe(board, row, col):
-    n = len(board)
 
+# A utility function to check if a queen can be placed on board[row][col].
+# (Assume that "col" queens are already placed in columns from 0 to col-1.)
+def is_safe(board, row, col):
+    n = len(board)
     # Check this row on left side
     for i in range(col):
         if board[row][i] == 1:
             return False
-
     # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1),
-                    range(col, -1, -1)):
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
     # Check lower diagonal on left side
-    for i, j in zip(range(row, n, 1),
-                    range(col, -1, -1)):
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
     return True
 
-def solveNQone(board, col):
-    n = len(board)
 
-    # base case: If all queens are placed
-    # then return true
+def solve_nqueens(board, col):
+    n = len(board)
+    # If all queens are placed, then return true
     if col >= n:
         return True
-
-    # Consider this column and try placing
-    # this queen in all rows one by one
+    # Consider this column and try placing this queen in all rows one by one
     for i in range(n):
-        if isSafe(board, i, col):
+        if is_safe(board, i, col):
             board[i][col] = 1  # Try to place a queen
-            if solveNQone(board, col + 1) == True:  # recurrent call to go deeper
+            if solve_nqueens(board, col + 1):  # recurrent call to go deeper
                 return True
             board[i][col] = 0  # If does not work, remove queen from board[i][col]
     return False  # Dead end: Cannot place anywhere in the column
 
-
-def setRowStarts(board):
-    n = len(board)
-    result = [-1]*n
-    for col in range(0,n):
-        for i in range(0,n):
-            if board[i][col] == 1:
-                result[col] = i
-    return result
-
-# A variant of solveNQone(...) finding all NQueens solutions.
-def solveNQall(board, col, allBoards):
-    n = len(board)
-    rowStarts = [-1] * n
-
-    # base case: If all queens are placed
-    # then return true
-    if col >= n:
-        rowStarts = setRowStarts(board)
-        allBoards.append(rowStarts)
-        return rowStarts
-
-    # Consider this column and try placing
-    # this queen in all rows one by one
-    for i in range(n):
-        if i < rowStarts[col] or ((i == rowStarts[col]) and (col == n-1)):
-            continue
-        if isSafe(board, i, col):
-            # Place this queen in board[i][col]
-            board[i][col] = 1
-            # recur to place rest of the queens
-            if solveNQall(board, col + 1, allBoards) == True:
-                return True
-            # If doesn't lead to a solution, remove queen from board[i][col]
-            board[i][col] = 0
-
-    # Dead end: The queen can not be placed in any row in this column
-    return False
-
-
-# Prints one of the feasible solutions.
-def printOne(n):
-    board = [[0]*n for k in range(0,n)]
-    if solveNQone(board, 0) == False:
-        print('Solution does not exist')
-        return False
-    printSolution(board)
-
-
-# Prints all feasible solutions
-def printAll(n):
-    allBoards = []  # all solutions stored in compact form
-    board = [[0]*n for k in range(0,n)]
-    solveNQall(board, 0, allBoards)
-    for bb in allBoards:
-        print(bb)
-
-    print('Total solutions: {}'.format(len(allBoards)))
